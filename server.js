dc2d535ce07eb217b63df3557daf4ffcf5dc3300
stefanocudini <stefano.cudini@gmail.com>
2020-10-06 15:42:32 +0200

    fix pause

--- server.js
+++ server.js
@@ -1,53 +1,64 @@
 
 const express = require('express');
-//const through = require('through');
 const path = require('path');
 
 const stream = require('youtube-audio-stream')
 //const decoder = require('lame').Decoder
-//const speaker = require('speaker')
 
 const app = express();
 const PORT = process.env.PORT;
 
-/*function write(buf) {
-    console.log('writing...');
-    this.queue('okkkk');
-}
-
-function end() {
-    this.queue(null);
-}
-
-var str = through(write, end);
-*/
+app.use((req, res, next) => {
+	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	console.log(new Date().toISOString(), ip, req.method, req.url, 'from:');
+	next();
+});
 
 app.get('/', function(req, res) {
-	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-	console.log('get index', ip);
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get(['/icon.svg','/favicon.ico'], function(req, res) {
+    res.sendFile(path.join(__dirname + '/icon.svg'));
+});
+
+app.get('/logo.svg', function(req, res) {
+    res.sendFile(path.join(__dirname + '/logo.svg'));
+});
 
 app.get('/stream', function(req, res) {
 
-	console.log('get stream', req.query.url);
- 	
+	var url = req.query.url;
+
+	if(!url) {
+		res.status(400).send('url param not found');
+		return
+	}
+
  	res.set({
-      //'Content-Type': 'audio/mp3',
+      //'Content-Type': 'audio/mpeg3',
       'Transfer-Encoding': 'chunked'
     });
 
-	stream(req.query.url)
-	//.pipe(decoder())
-	.pipe(res)
-	//var s = req.pipe(str).pipe(res);
-	//.pipe(new speaker())
+	try {
 
+		stream(url).pipe(res).on('error', function(e) {
+			console.log('stream error', e)
+		});
+		//.pipe(decoder())
+	}
+	catch(e) {
+
+		console.log('stream exception', e);
+		res.removeHeader('Transfer-Encoding');
+		res.set('Connection', 'close');
+		res.status(400).send('url error')
+		res.end();
+	}
 });
 
 
 /* listen */
 app.listen(PORT, function () {
-    console.log('listening on port ' + PORT + '...');
+    console.log(new Date().toISOString(), 'listening on port',PORT);
 });
